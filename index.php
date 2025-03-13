f2436f29845a9cb5ca454af274db5f7fa45b6486
G. vanTil <129494109+Mem8or@users.noreply.github.com>
2025-03-13 12:30:36 +0100

    Setup includes

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Portfolio</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <nav>
+
+        </nav>
+        
+    </body>
+    <footer>
+        
+    </footer>
+</html>
