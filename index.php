7dc04439edb69d4a539e863c5fe03cc5575e65d8
G. vanTil <129494109+Mem8or@users.noreply.github.com>
2025-03-14 15:09:02 +0100

    note

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <html>
     <head>
         <title>Portfolio</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
-        <nav>
+        <nav id="menu">
             menu here
         </nav>
         <div id="intro">
@@ -15,6 +15,17 @@
         </div>
         <div id="projects">
             projects here
+            <?php include("./includes/projects.php");?>
+        </div>
+        <div id="contact">
+            <form method="post">
+                <label for="name">Naam: </label> <br>
+                <input type="text" name="name"> <br>
+                <label for="email">E-mail: </label> <br>
+                <input type="email" name="email"> <br>
+                <label for="message">Bericht: </label> <br>
+                <textarea name="message" id="message" rows="20" cols="50"></textarea>
+            </form>
         </div>
     </body>
     <footer>
