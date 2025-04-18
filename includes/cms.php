efe66b4e40871b48d80b10257b18523117960db7
G. vanTil <129494109+Mem8or@users.noreply.github.com>
2025-04-18 14:20:44 +0200

    note

--- includes/cms.php
+++ includes/cms.php
@@ -5,6 +5,7 @@ $pass = 'gert2002';
 $db = 'portfolio-website';
 $output = '';
 $clearance = false;
+$submitstate = '';
 
 session_set_cookie_params([
     'lifetime' => 100,
@@ -12,9 +13,26 @@ session_set_cookie_params([
 ]);
 
 session_start();
+
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header('Location: login.php');
     exit;
+}
+
+
+// clearance check
+if ($_SESSION['clearance'] == true){
+    $clearance = true;
+} else{
+    $clearance = false;
+}
+
+// uitzetten van submit en delete buttons als clearance false is
+if($clearance == false){
+    $submitstate = 'disabled';
+}
+if($clearance == true){
+    $submitstate = '';
 }
 
 $dbh = new mysqli($server, $user, $pass, $db);
@@ -23,25 +41,32 @@ if ($dbh->connect_error){
 } else{
     $dbh->set_charset('utf8');
 
+
+    // ophalen van messages en het in een array zetten
     $messagequery = "SELECT * FROM messages";
     $messages = $dbh->query($messagequery);
 
+    // ophalen van projects en het in een array zetten
     $projectsResult = $dbh->query("SELECT * FROM projects");
     $linksResult = $dbh->query("SELECT * FROM project_links");
 
     $projectLinks = [];
-while ($linkRow = $linksResult->fetch_assoc()) {
+    while ($linkRow = $linksResult->fetch_assoc()) {
     $projectId = $linkRow['project_id'];
+
     if (!isset($projectLinks[$projectId])) {
         $projectLinks[$projectId] = [];
     }
-    $projectLinks[$projectId][] = $linkRow['link'];
-}
 
-$projects = [];
-while ($row = $projectsResult->fetch_assoc()) {
+    $projectLinks[$projectId][] = $linkRow['link'];
+    }
+
+    $projects = [];
+    while ($row = $projectsResult->fetch_assoc()) {
     $projectId = $row['id'];
+
     $projects[] = [
+        'id' => $row['id'],
         'image' => $row['image'],
         'imageAlt' => $row['imageAlt'],
         'title' => $row['title'],
@@ -53,7 +78,7 @@ while ($row = $projectsResult->fetch_assoc()) {
 }
 
 
-if (isset($_POST['Logout'])){
+if (isset($_POST['logout'])){
 
     $_SESSION['loggedin'] = false;
 
@@ -71,43 +96,72 @@ if (isset($_POST['Logout'])){
         <link rel="stylesheet" href="../css/cmsstyle.css">
     </head>
 
-    <div>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <div id="User">
         <h1>Hi <?= htmlspecialchars($_SESSION['username']) ?></h1>
+        <p>Your clearance is: <?= $clearance?></p>
 
         <form action="../index.php" method="POST">
-            <input name="Log out" type="submit" value="Logout" class="button">
+            <input name="logout" id="logout" type="submit" value="Logout">
         </form>
     </div>
 
 
     <div id="wrapper">
         <div id="content" class="column">
-            <h2>Content:</h2>
+            <div class="titlebar">
+                <h2>Content:</h2>
+                <hr>
+            </div>
+            
             <?php
 
                 if(!empty($projects)){    
                     foreach($projects as $project){
                         $links = '';
                         foreach($project['links'] as $link){
-                            $links .= '<input type="text" value="'. $link .'"><br>';
+                            $links .= '<label for="link">link: </label><br><input type="text" value="'. $link .'" name="link"><br>';
                         }
                         
-                    $output .= '<form method="post">';
-            
-                    $output .= '<input type="text" value="'. $project['image'] .'"><br><input type="text" value="'. $project['imageAlt'].'"><br><input type="text" value="'. $project['title'].'"><br><textarea>'. $project['description'].'</textarea><br>';
-                    $output .= '<div>'. $links .'</div>
-                    <textarea>'. $project['content'] .'</textarea><br><br>';
-                    $output .= '<form>';
+                    $output .= '<div class="formwrapper">
+
+                    <h2>Project '.$project['id'].': </h2> <h3>'. $project['title'].'</h3>
+
+                    <form method="post" name="'. $project['id'] .'">
+
+                    <label for="img">Image src: </label><br>
+                    <input type="text" name="img" value="'. $project['image'] .'"><br>
+
+                    <label for="imgalt">Image description: </label><br>
+                    <input type="text" name="imgalt" value="'. $project['imageAlt'].'"><br>
+
+                    <label for="title">Title: </label><br>
+                    <input type="text" name="title" value="'. $project['title'].'"><br>
+
+                    <label for="projectdesc">Project description: </label><br>
+                    <textarea name="projectdesc">'. $project['description'].'</textarea><br>
+
+                    <div>'. $links .'</div>
+
+                    <label for="content">Content: </label><br>
+                    <textarea name="content">'. $project['content'] .'</textarea><br>
+
+                    <div class="buttonrow">
+                    <input type="submit" class="button" name="submit-button" value="Push changes" '. $submitstate .'>
+                    <input type="submit" class="deletebutton" onclick="return confirm(\'Are you sure you want to delete this entry?\');" name="delete-button" value="" '. $submitstate .'>
+                    </div>
+                    </form></div>';
                 }};
 
                 echo $output;
             ?>
         </div>
         <div id="messages" class="column">
-            <h2>Messages:</h2>
+        <div class="titlebar">
+                <h2>Messages:</h2>
+                <hr>
+            </div>
         </div>
     </div>
-
-
-
 </html>
--- includes/login.php
+++ includes/login.php
@@ -1,37 +1,53 @@
 <?php
+    $server = 'localhost';
+    $user = 'GertvanTil';
+    $pass = 'gert2002';
+    $db = 'portfolio-website';
+
     session_start();
 
-    $users = [
-        'admin' => ['password' => 'iamadministrator'],
-        'guest' => ['password' => 'guest']
-    ];
+    $dbh = new mysqli($server, $user, $pass, $db);
+    if ($dbh->connect_error){
+        die('Verbinden mislukt: '.$dbh->connect_error);
+    } else{
+        $dbh->set_charset('utf8');
+    }
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        if(isset($users[$username]) && $users[$username]['password'] == $password){
-            $_SESSION['loggedin'] = true;
-            $_SESSION['username'] = $username;
-            header('Location: cms.php');
-            exit;
-        } else {
-            echo 'Invalid username or password!';
+        $stmt = $dbh->prepare("SELECT username, password, clearance FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $user = $result->fetch_assoc();
+
+            if ($user['password'] === $password){
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['clearance'] = $user['clearance'];
+                header('Location: cms.php');
+                exit;
+            } else {
+                echo 'Invalid username or password!';
+            }
         }
-    }
 ?>
 <html>
     <head>
         <title>Login</title>
+        <link rel="stylesheet" href="../css/cmsstyle.css">
     </head>
     <body>
     <h2>Login</h2>
         <form action="login.php" method="POST">
-            <label for="username">Username:</label>
+            <label for="username">Username:</label><br>
             <input type="text" name="username" id="username" required><br><br>
-            <label for="password">Password:</label>
+            <label for="password">Password:</label><br>
             <input type="password" name="password" id="password" required><br><br>
-            <input type="submit" value="Login">
+            <input type="submit" value="Login" class="button">
         </form>
     </body>
 </html>
